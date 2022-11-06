f9faa115920820df84e23e576dc8eaa99f00ec4b
Cedric Mendoza <cedric.mendoza02@gmail.com>
2022-11-06 08:57:10 +0800

    Merge all tech skills

--- src/components/About.js
+++ src/components/About.js
@@ -6,16 +6,12 @@ const About = () => {
   return (
     <Card title="About Me..." customStyle="lg:w-screen 2xl:w-3/5 mx-auto">
         <div className="h-1/2 text-sm sm:text-base lg:text-2xl flex flex-col sm:flex-row items-center gap-3 lg:gap-x-8 2xl:gap-x-10 p-3">
-            <img src={self} className="rounded-full w-1/3 max-w-sm" />
-            <p className="">I am a self-taught developer with 1 year of experience programming for web and some mobile apps on the side.
+            <img src={self} className="rounded-full w-1/3 max-w-sm shadow-lg" />
+            <p className="">I've built myself from the ground up and have taught myself web development using all the resources I could find.
                <br />
                <br />
-               I have a good grasp of web technologies specifically Javascript, HTML and CSS. I've mainly focused on ReactJS framework for my apps and is working on
-               integrating some back end to existing apps. Skills I'm working on right now is
-               React Native, creating my own assets, SQL, PHP, Design in general and back end languages, working my way to develop full stack apps.
-               <br />
-               <br />
-               I hope that you will consider having me on your company and I'd be proud to show you that I'm passionate on learning and doing my best on this field.
+               I have a good working knowledge of the MERN(MongoDB, Express, ReactJS, NodeJS) stack.
+
             </p>
         </div>
     </Card>
--- src/components/TechSkills.js
+++ src/components/TechSkills.js
@@ -8,7 +8,6 @@ const TechSkills = () => {
   return (
     <Card title="Tools I use..." customStyle="lg:w-screen 2xl:w-3/5 mx-auto">
         <div className="my-3">
-            <SectionSubHeader text="Primary" />
             <ul className="grid grid-cols-3 gap-5 sm:gap-2 justify-items-center">
                 <li className={styles.item}><SiHtml5 className={styles.icons}/>HTML5</li> 
                 <li className={styles.item}><SiCss3 className={styles.icons}/>CSS3</li> 
@@ -17,16 +16,11 @@ const TechSkills = () => {
                 <li className={styles.item}><SiTailwindcss className={styles.icons}/>TailwindCSS</li>
                 <li className={styles.item}><SiWebpack className={styles.icons}/>Webpack</li>
                 <li className={styles.item}><BsGithub className={styles.icons}/>GitSCM</li>
-            </ul>
-        </div>
-        <div className="my-3">
-            <SectionSubHeader text="Others" />
-            <ul className="grid grid-cols-3 gap-5 sm:gap-2 justify-items-center">
                 <li className={styles.item}><TbBrandReactNative className={styles.icons}/>ReactNative</li>
                 <li className={styles.item}><SiJava className={styles.icons}/>Java</li> 
                 <li className={styles.item}><SiKotlin className={styles.icons}/>Kotlin</li>
                 <li className={styles.item}><BsBarChartFill className={styles.icons}/>D3js</li>
-                <li className={styles.item}><SiMongodb className={styles.icons}/>MongoDB</li>   
+                <li className={styles.item}><SiMongodb className={styles.icons}/>MongoDB</li>
             </ul>
         </div>
     </Card>
